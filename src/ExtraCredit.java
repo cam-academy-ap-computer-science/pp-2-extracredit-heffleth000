efb07c0e89f487e7ec30179f9f2f2e03be3826e3
heffleth000 <heffleth000@CAM-LAB-28.bgsd.k12.wa.us>
2019-10-15 07:48:34 -0700

    Working on top half

--- src/ExtraCredit.java
+++ src/ExtraCredit.java
@@ -2,7 +2,46 @@
 public class ExtraCredit {
 
 	public static void main(String[] args) {
-
+		tip();
+		line();
+		top();
+	/*	line();
+		bottom();
+		line();
+		smoke(); */
 	}
-
-}
+	//will print the line of = and * printed multiple times in rocket - works
+	public static void line() {
+		System.out.print("+");
+		for (int x = 1; x <= 6; x++) {
+			System.out.print("=*");
+		}
+		System.out.println("+");
+	}
+	//creates tip of rocket - works
+	public static void tip() {
+		for (int i = 1; i <= 5; i ++) {
+			for (int space = 5; space >= i; space--) {		//prints out spaces
+				System.out.print(" ");
+			}
+			for (int fslash = 1; fslash <= i; fslash++) {	//prints out forward slashes
+				System.out.print("/");
+			}
+			System.out.print("**");							//prints out stars in middle
+			for (int bslash = 1; bslash <= i; bslash++) {	//prints out backslashes
+				System.out.print("\\");
+			}
+			System.out.println("");
+		}
+	}
+	//creats top half of rocket
+	public static void top() {
+		for (y = 1; y <= 6; y++) {
+			System.out.print("|");;
+			for (dot = 2; dot >= 1; dot--) {
+				System.out.print(".");
+			}
+		}
+	}
+	
+	
